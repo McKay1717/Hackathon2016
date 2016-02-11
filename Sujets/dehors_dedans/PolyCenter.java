d549cdb06840e84445981db25e2c9f7e676a37b8
McKay1717 <nicolas1706ni@gmail.com>
2016-02-11 13:49:32 +0100

    Usage de tableau interne

--- Sujets/dehors_dedans/PolyCenter.java
+++ Sujets/dehors_dedans/PolyCenter.java
@@ -3,59 +3,66 @@ import java.io.*;
 import java.util.*;
 
 class Polygon {
-    
-    int nbVertices; // nb sommets
-    double[] x; // les coordonnées en x de chaque sommet
-    double[] y; // les coordonnées en y de chaque sommet
 
-    // autres attributs ???
-    
-    public Polygon() {
-	// ???
-    }
-    
-    public Point Barycentre(ArrayList<Point> list)
-    {
-    	   double X = 0, Y = 0;
+	int nbVertices; // nb sommets
+	double[] x; // les coordonnées en x de chaque sommet
+	double[] y; // les coordonnées en y de chaque sommet
 
-           for(Point knot : list) {
-               X += knot.getX();
-               Y += knot.getY();
-           }
-       return new Point((int)(X / list.size()), (int) (Y / list.size()));
-    }
+	// autres attributs ???
 
-    /* readParameters():
-       permet de lire avec scan les paramètres du polygone :
-       le nombre de sommets suivi des coordonnées x et y de chacun
-     */
-    public void readParameters(Scanner scan) {
+	public Polygon() {
+		// ???
+	}
 
-    }
+	public double BarycentreX() {
+		double X = 0;
+		for (int i = 0; i < x.length; i++) {
+			X += x[i];
+		}
 
-    // autres méthodes ???
+		return X / x.length;
+	}
+
+	public double BarycentreY() {
+		double Y = 0;
+		for (int i = 0; i < y.length; i++) {
+			Y += y[i];
+		}
+
+		return Y / y.length;
+	}
+
+
+	/*
+	 * readParameters(): permet de lire avec scan les paramètres du polygone :
+	 * le nombre de sommets suivi des coordonnées x et y de chacun
+	 */
+	public void readParameters(Scanner scan) {
+
+	}
+
+	// autres méthodes ???
 }
 
-    
 class PolyCenter {
 
-    public static void main(String[] args) {
-	Locale.setDefault(Locale.ENGLISH);
-	
-	Scanner scan = new Scanner(System.in);
-	int nbPoly = scan.nextInt();
-	
-	for(int i=0;i<nbPoly;i++) {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.ENGLISH);
 
-	    Polygon p = new Polygon();
-	    p.readParameters(scan);
+		Scanner scan = new Scanner(System.in);
+		int nbPoly = scan.nextInt();
 
-	    /* A COMPLETER : (cf. sujet)
+		for (int i = 0; i < nbPoly; i++) {
 
-	       afficher sur la sortie standard le nombre
-	       de sommets puis yes ou no selon que le centre du
-	       polygone est à l'intérieur ou non.
-	    */
+			Polygon p = new Polygon();
+			p.readParameters(scan);
+
+			/*
+			 * A COMPLETER : (cf. sujet)
+			 * 
+			 * afficher sur la sortie standard le nombre de sommets puis yes ou
+			 * no selon que le centre du polygone est à l'intérieur ou non.
+			 */
+		}
 	}
-    }
 }
